8b274e5e18a9a87f9a1d662d945d3827f64bebfd
so23go <so23go@naver.com>
2024-01-29 22:11:46 +0900

    객체 설계 1,2

---
+++ src/main/java/org/study/chapter1/TicketSeller.java
@@ -0,0 +1,13 @@
+package org.study.chapter1;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
