7146d59c125c3469364656d00d9e39ded8ae68c8
so23go <so23go@naver.com>
2024-01-29 21:39:24 +0900

    Project 생성

---
+++ src/main/java/org/study/chapter1/Bag.java
@@ -0,0 +1,40 @@
+package org.study.chapter1;
+
+public class Bag {
+    private Long amount;
+
+    private Invitation invitation;
+
+    private Ticket ticket;
+
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return ticket != null;
+
+
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+
+    public Bag(Long amount) {
+        this(null, amount);
+    }
+
+    public Bag(Invitation invitation, Long amount) {
+        this.amount = amount;
+        this.invitation = invitation;
+    }
+}
---
+++ src/main/java/org/study/chapter1/Ticket.java
@@ -0,0 +1,9 @@
+package org.study.chapter1;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
